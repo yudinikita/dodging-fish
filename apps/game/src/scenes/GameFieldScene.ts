40a84a980a3038156e2077974b78103d03785728
Nikita Yudin <nikitayudin782@gmail.com>
2022-08-02 13:32:09 +0300

    fix(game): change color with 40-45 score

--- apps/game/src/scenes/GameFieldScene.ts
+++ apps/game/src/scenes/GameFieldScene.ts
@@ -240,7 +240,7 @@ export default class GameFieldScene extends Phaser.Scene {
     if (score < 30) return { background: 0x76_76_75, spike: 0xfc_f9_fe }
     if (score < 35) return { background: 0x2b_69_7f, spike: 0x63_d1_f8 }
     if (score < 40) return { background: 0x1e_72_00, spike: 0x6e_cf_00 }
-    if (score < 45) return { background: 0x00_29_8a, spike: 0x00_69_f5 }
+    if (score < 45) return { background: 0x00_29_8a, spike: 0x32_8a_ff }
     if (score < 50) return { background: 0x8c_0f_3e, spike: 0xfe_1f_67 }
     if (score < 55) return { background: 0xff_a9_2c, spike: 0xff_ff_ff }
     if (score < 60) return { background: 0x00_97_ff, spike: 0xff_ff_ff }
